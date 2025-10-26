e015b2a250053a0cdd6c9b14a39e8d44e4780196
Grinder21 <spartawinner280@mail.ru>
2025-10-26 20:50:06 +0300

    add: add sockets connection, message, disconnect

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,32 @@
-import express, { Application, Request, Response } from "express";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
-const app: Application = express();
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("message", (msg) => {
+    console.log("Message received:", msg);
+    io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+app.use(cors());
+
 const PORT = 5000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript + Express!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+server.listen(PORT, () => console.log(`🚀 Server listening on port ${PORT}`));
